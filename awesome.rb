b244d63f31a009e3852390a62ed79fd10d629477
Mike Neumegen <mike@cloudcannon.com>
2016-03-22 15:15:31 -0700

    cleaned up ruby code

--- awesome.rb
+++ awesome.rb
@@ -4,17 +4,12 @@ input = JSON.parse(ARGF.read)
 
 start = Time.now
 
-10000.times.each do |e|
-	min_column = input.map{|a| a[0]}
-	max = min_column.max
-	second_smallest = min_column.sort[1]
-	min = input[min_column.index(min_column.min)].max_by do |x|
-		if x > second_smallest
-			x = -1
-		end
-		x
-	end
+1.times.each do |e|
+  min_column = input.map {|a| a.first}
+  max = min_column.max
+  second_smallest = min_column.sort[1]
+  min_index = min_column.index(min_column.min)
+  min = input[min_index].max_by { |x| x > second_smallest ? -1 : x }
 end
-
 
 puts (Time.now - start).to_f
