faa81e353050b87c4249eec47e1f9912f7450bb0
andy grosser <dioptre@gmail.com>
2016-03-26 19:33:09 -0700

    updated python

--- python/island.py
+++ python/island.py
@@ -5,10 +5,10 @@ import time
 N = 1000000
 
 def Compute(_):
-for i in range(25):
-max_left = np.maximum.accumulate(heights)
-max_right = np.maximum.accumulate(heights[::-1])[::-1]
-np.sum(np.maximum(0, np.minimum(max_left, max_right) - heights))
+    for i in range(25):
+        max_left = np.maximum.accumulate(heights)
+        max_right = np.maximum.accumulate(heights[::-1])[::-1]
+        np.sum(np.maximum(0, np.minimum(max_left, max_right) - heights))
 
 heights = np.random.randint(100, size=N)
 
